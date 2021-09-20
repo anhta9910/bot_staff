416b1411ed9a1acc1b2ed04db064311b4840dd5a
H Anh <anhta.9910@gmail.com>
2021-09-20 23:32:33 +0700

    change api anhth.xyz

--- commands/fun/girls.js
+++ commands/fun/girls.js
@@ -5,15 +5,13 @@ module.exports = {
   run: async (client, message, args) => {
     const option = {
       method: "GET",
-      url: "https://anhth.xyz/girls.json",
+      url: "https://anhth.xyz/346923913717647.json",
     };
     await axios
       .request(option)
       .then((res) => {
-        const count = Math.floor(Math.random() * 34018);
-        console.log(count);
+        const count = Math.floor(Math.random() * 2144);
         const url = res.data[count].url;
-        console.log(url);
         const embed = new MessageEmbed().setImage(url).setColor("Blue");
         message.channel.send(embed);
       })
